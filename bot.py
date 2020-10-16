d0ece59811720445cec4367d2c5d15cfe57b1e27
Andromeda606 <43077625+Andromeda606@users.noreply.github.com>
2020-10-16 18:26:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,113 +3,103 @@ import time
 import json
 import random
 
-#Import Kısmı
+#Import
 
-#Json Dosyasından Veri Aktarılıyor
 data = open("settings.json", "r+", encoding="utf8")
 jsondata = json.load(data)
 data.close()
 
-emailEnd = jsondata["eMailEnd"] #eMail sonu çekiliyor
-passw = jsondata["password"] #Şifre Çekiliyor
-email = jsondata["email"] #eMail çekiliyor
-emailFull = email + "+" + str(random.randint(1, 99999)) + "@" + emailEnd #eMail toplanıyor
-browser = webdriver.Firefox(executable_path=jsondata["geckoPath"]) #Gecko konumu alınıyor
-browser.implicitly_wait(10) #10 sn içinde istediğimiz ortaya çıkmaz ise hata ver
+emailEnd = jsondata["eMailEnd"]
+passw = jsondata["password"]
+email = jsondata["email"]
+emailFull = email + "+" + str(random.randint(1, 99999)) + "@" + emailEnd
+browser = webdriver.Firefox(executable_path=jsondata["geckoPath"])
+browser.implicitly_wait(10)
 
-#Fonksiyonların bulunduğu yer
-def gMailVeriAktar():
-    import gmailReader as gr  # mail readeri ediniyoruz
-    code = gr.getmail() #maili çekiyoruz
-    codePath = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/form/div[4]/div[5]/div/input') #kod konumu
-    if not(codePath.get_property("value") == code): #Eğer kod aynı değil ise ekrana yazsın
+#Functions
+def getGmail():
+    import gmailReader as gr
+    code = gr.getmail()
+    codePath = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/form/div[4]/div[5]/div/input')
+    if not(codePath.get_property("value") == code):
         try:
             codePath.clear()
         except:
-            print("Silme işlemi gerçekleştirilemedi")
+            print("Deletion could not be performed")
         codePath.send_keys(code)
-        kayitOlBas()
-    else: #Aynı ise bunu tekrar açsın
+        Register()
+    else:
         if browser.current_url is None:
             exit()
         else:
-            gMailVeriAktar()
+            getGmail()
 
 
-def kayitOlBas():
+def Register():
     global emailFull,passw
     try:
         browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/form/div[4]/button').click()
-        time.sleep(1) #Sonucu bekleyelim
-        sonuc = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/form/div[4]/div[6]/div')
-        if sonuc.get_attribute("innerHTML")== "Incorrect code":
-            #Kod yanlış ise TikToku dinleyip tekrar kod istiyoruz
+        time.sleep(1)
+        result = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/form/div[4]/div[6]/div')
+        if result.get_attribute("innerHTML")== "Incorrect code":
             time.sleep(1)
-            gMailVeriAktar()
-        elif sonuc.get_attribute("innerHTML") == "Verification failed. Please click Resend and try again.":
-            print("Kodu girmediniz, Birdaha deneniyor")
+            getGmail()
+        elif result.get_attribute("innerHTML") == "Verification failed. Please click Resend and try again.":
+            print("Verification failed. Please click Resend and try again.")
             time.sleep(1)
-            kayitOlBas()
+            Register()
         else:
             try:
                 skipPath = browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/button[2]")
-                print("hesap oluştu, mailler siliniyor ve yeni hesaba geçiliyor")
-                skipPath.click() #Skipe basılıyor
-                #basarilikayit(emailFull, passw) #Dosyaya yazılıyor
-                import gmailReader as gr #Mailleri siliyoruz
-                gr.deletemail() #Mailleri silmeyi emrettik
-                browser.quit() #İşimiz burda bitiyor
-            except:
-                print("hesap açılmadı")
+                print("The account has been created, the mails are deleted and the new account is transferred")
+                skipPath.click()
 
-        #TODO ELSE BURADA OLACAK VE BURDA KAYIT OLMA TAMAMLANDIĞINDA OLACAKLAR OLACAK!
+                import gmailReader as gr
+                gr.deletemail()
+                browser.quit()
+            except:
+                print("no account opened")
     except:
         try:
             print(browser.current_url)
         except:
             import bot
             exit()
-        #Eğer ekrana browser ismi yazılmıyor ise çıksın, Program işi bittiği halde açık kalmasın
         try:
             skipPath = browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/button[2]")
-            print("hesap oluştu, mailler siliniyor ve yeni hesaba geçiliyor")
-            skipPath.click()  # Skipe basılıyor
-            basarilikayit(emailFull, passw) #Dosyaya yazılıyor
-            import gmailReader as gr  # Mailleri siliyoruz
-            gr.deletemail()  # Mailleri silmeyi emrettik
-            browser.quit()  # İşimiz burda bitiyor
+            print("The account has been created, the mails are deleted and the new account is transferred")
+            skipPath.click()
+            successReg(emailFull, passw)
+            import gmailReader as gr
+            gr.deletemail()
+            browser.quit()
             import os,sys
             os.startfile(__file__)
             sys.exit()
         except:
             if(browser.current_url=="https://www.tiktok.com/login/download-app"):
-                print("HESAP YARI DOGA DÜŞTÜ")
-                basarilikayit(emailFull, passw)  # Dosyaya yazılıyor
-                import gmailReader as gr  # Mailleri siliyoruz
-                gr.deletemail()  # Mailleri silmeyi emrettik
-                browser.quit()  # İşimiz burda bitiyor
+                successReg(emailFull, passw)
+                import gmailReader as gr
+                gr.deletemail()
+                browser.quit()
                 import os, sys
                 os.startfile(__file__)
                 sys.exit()
-            print("")
-            print("Kodu girmediniz, Birdaha deneniyor")
+            print("You did not enter the code, try again")
             time.sleep(1)
-            kayitOlBas()
+            Register()
 
-def basarilikayit(email, password):
+def successReg(email, password):
     veri = open("users.txt","a")
     veri.write(email+":"+password+"\n")
-#Fonksiyonlar bitti
+#Functions end
 
-#VERİLER AKTARILDI, PROGRAM BAŞLIYOR
-
-browser.get("https://www.tiktok.com/signup/phone-or-email/email?lang=en") #Kayıt olma kısmına giriyor
+browser.get("https://www.tiktok.com/signup/phone-or-email/email?lang=en")
 emailPath = browser.find_element_by_name("email")
 passPath = browser.find_element_by_name("password")
-#eMail ve Pass konumu alınıyor
+
 emailPath.send_keys(emailFull)
 passPath.send_keys(passw)
-browser.get('javascript:document.getElementsByClassName("select-container-1DrEI")[0].click();document.getElementsByClassName("list-item-2xmNY")[0].click();document.getElementsByClassName("select-container-1DrEI")[1].click();document.getElementsByClassName("list-item-2xmNY")[1].click();document.getElementsByClassName("select-container-1DrEI")[2].click();document.getElementsByClassName("list-item-2xmNY")[40].click();document.getElementsByClassName("login-button-86o6Z")[0].click();')
-gMailVeriAktar()
-#Veriler yazılıyor
+browser.get('javascript:document.getElementsByClassName("select-container-36J-Y")[0].click();document.getElementsByClassName("list-item-26TR9")[0].click();document.getElementsByClassName("select-container-36J-Y")[1].click();document.getElementsByClassName("list-item-26TR9")[1].click();document.getElementsByClassName("select-container-36J-Y")[2].click();document.getElementsByClassName("list-item-26TR9")[40].click();document.getElementsByClassName("login-button-Rt4Hk")[0].click();')
+getGmail()
 
