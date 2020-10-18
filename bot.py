e4042694657c79544fbab691be388c2ca17a2d9c
Andromeda606 <43077625+Andromeda606@users.noreply.github.com>
2020-10-18 13:01:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import time
 import json
 import random
 
-#Import
+# Import
 
 data = open("settings.json", "r+", encoding="utf8")
 jsondata = json.load(data)
@@ -13,15 +13,20 @@ emailEnd = jsondata["eMailEnd"]
 passw = jsondata["password"]
 email = jsondata["email"]
 emailFull = email + "+" + str(random.randint(1, 99999)) + "@" + emailEnd
-browser = webdriver.Firefox(executable_path=jsondata["geckoPath"])
+options = webdriver.FirefoxOptions()
+#options.add_argument('-headless')
+browser = webdriver.Firefox(executable_path=jsondata["geckoPath"],options=options)
+#browser = webdriver.Chrome(executable_path=jsondata["chromePath"])
 browser.implicitly_wait(10)
 
-#Functions
+
+# Functions
 def getGmail():
     import gmailReader as gr
+    print("getMail")
     code = gr.getmail()
     codePath = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/form/div[4]/div[5]/div/input')
-    if not(codePath.get_property("value") == code):
+    if not (codePath.get_property("value") == code):
         try:
             codePath.clear()
         except:
@@ -36,18 +41,20 @@ def getGmail():
 
 
 def Register():
-    global emailFull,passw
+    global emailFull, passw
     try:
         browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/form/div[4]/button').click()
         time.sleep(1)
         result = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/form/div[4]/div[6]/div')
-        if result.get_attribute("innerHTML")== "Incorrect code":
+        if result.get_attribute("innerHTML") == "Incorrect code":
             time.sleep(1)
             getGmail()
         elif result.get_attribute("innerHTML") == "Verification failed. Please click Resend and try again.":
             print("Verification failed. Please click Resend and try again.")
             time.sleep(1)
             Register()
+        elif result.get_attribute("innerHTML") == "Verification failed. Please click Resend and try again.":
+            print("YOUR IP BLOCKED!")
         else:
             try:
                 skipPath = browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/button[2]")
@@ -73,11 +80,11 @@ def Register():
             import gmailReader as gr
             gr.deletemail()
             browser.quit()
-            import os,sys
+            import os, sys
             os.startfile(__file__)
             sys.exit()
         except:
-            if(browser.current_url=="https://www.tiktok.com/login/download-app"):
+            if (browser.current_url == "https://www.tiktok.com/login/download-app"):
                 successReg(emailFull, passw)
                 import gmailReader as gr
                 gr.deletemail()
@@ -89,17 +96,26 @@ def Register():
             time.sleep(1)
             Register()
 
-def successReg(email, password):
-    veri = open("users.txt","a")
-    veri.write(email+":"+password+"\n")
-#Functions end
 
-browser.get("https://www.tiktok.com/signup/phone-or-email/email?lang=en")
+def successReg(email, password):
+    veri = open("users.txt", "a")
+    veri.write(email + ":" + password + "\n")
+
+
+# Functions end
+cookies = "tt_webid=6884877451467179525; tt_webid_v2=6884877451467179525; s_v_web_id=verify_kgev18p4_wS31yIAd_DbQv_4d9Y_944R_3giruvGhbAwM; _ga=GA1.2.1495663218.1603010452; _gid=GA1.2.224102598.1603010452; passport_csrf_token=1686c19f83b8da0c0dbf1634f55f933c; odin_tt=e93bab047745d7a898fcdd155f575acb181bd17df20c8d98bc5963ad3292070aaf51b5e4388c883a8c17965c0dd5b852532e783b6b792ea69624dcff21466a6b; store-idc=maliva; store-country-code=tr; sid_guard=e41bbe2a5233598221d4d35fed7b67cb%7C1603011367%7C21600%7CSun%2C+18-Oct-2020+14%3A56%3A07+GMT; uid_tt=8492306fe5c1e3c2b593491b1a2b9c21; uid_tt_ss=8492306fe5c1e3c2b593491b1a2b9c21; sid_tt=e41bbe2a5233598221d4d35fed7b67cb; sessionid=e41bbe2a5233598221d4d35fed7b67cb; sessionid_ss=e41bbe2a5233598221d4d35fed7b67cb; MONITOR_WEB_ID=6884877451467179525".split(
+    ";")
+
+
+browser.get("https://www.tiktok.com/signup/phone-or-email/email")
 emailPath = browser.find_element_by_name("email")
 passPath = browser.find_element_by_name("password")
+#for i in cookies:
+#    cookie = i.split("=")
+#    browser.add_cookie({"name": cookie[0], "value": cookie[1]})
 
 emailPath.send_keys(emailFull)
 passPath.send_keys(passw)
-browser.get('javascript:document.getElementsByClassName("select-container-36J-Y")[0].click();document.getElementsByClassName("list-item-26TR9")[0].click();document.getElementsByClassName("select-container-36J-Y")[1].click();document.getElementsByClassName("list-item-26TR9")[1].click();document.getElementsByClassName("select-container-36J-Y")[2].click();document.getElementsByClassName("list-item-26TR9")[40].click();document.getElementsByClassName("login-button-Rt4Hk")[0].click();')
+browser.get(
+    'javascript:document.getElementsByClassName("select-container-36J-Y")[0].click();document.getElementsByClassName("list-item-26TR9")[0].click();document.getElementsByClassName("select-container-36J-Y")[1].click();document.getElementsByClassName("list-item-26TR9")[1].click();document.getElementsByClassName("select-container-36J-Y")[2].click();document.getElementsByClassName("list-item-26TR9")[40].click();document.getElementsByClassName("login-button-Rt4Hk")[0].click();')
 getGmail()
-
